9f515ff8b85c414791cecc9414b80d4c891d0fa8
LAPTOP-KCAUDTIJ\FIRDAUS <firdauschuzaeni@gmail.com>
2022-12-07 20:35:33 +0700

    Three Commit

--- Minggu 14/movie/start.sh
+++ Minggu 14/movie/start.sh
@@ -2,14 +2,14 @@ set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV=.data/env
+VIRTUALENV=.data/venv
 
-if [ ! -d $VIRTUALENV]; then
-    python3 -m venv $VIRTUALENV
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
 fi
 
-if [ ! -f $VIRTUALENV/bin/pip]; then
-    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
